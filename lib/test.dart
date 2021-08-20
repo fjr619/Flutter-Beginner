0ce2582e1043e11dd1a574ecb40d796a44ddf34e
Franky Wijanarko <frank.jr.619@gmail.com>
2021-08-20 16:05:05 +0700

    dart tutorial : if, switch case

--- lib/test.dart
+++ lib/test.dart
@@ -2,7 +2,27 @@ import 'dart:io';
 import 'package:flutter_beginner/test.dart';
 
 main(List<String> argumets) {
-  String input = stdin.readLineSync();
-  int number = int.tryParse(input);
-  print('Helo world! ${number + 10}');
+  int number = int.tryParse(stdin.readLineSync());
+  // if (number > 0) {
+  //   print("positive");
+  // } else if (number < 0) {
+  //   print("negative");
+  // } else {
+  //   print("nol");
+  // }
+
+  String output;
+  output = (number > 0) ? "positive" : "negative atau nol";
+  print(output);
+
+  switch(number) {
+    case 1:
+      print("satu");
+      break;
+    case 2:
+      print("dua");
+      break;
+    default:
+      print("bilangan lain");
+  }
 }
